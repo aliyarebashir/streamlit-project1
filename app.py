c781506283f8232691f19d779a6a82eb032ed17e
uwes <uwess529300@gmail.com>
2023-09-06 17:31:38 +0300

    changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import os
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Deja vu Dashboard", page_icon="gem", layout="centered")
 path=os.path.dirname(__file__)
@@ -114,3 +115,50 @@ elif selection=="Bussiness snapshot":
         plt.ylabel("Revenue")
         st.pyplot(plt)
 
+footer_temp = """
+<!-- CSS  -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" 
+type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="static/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" 
+integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<footer class="page-footer grey darken-4">
+<div class="container" id="aboutapp">
+<div class="row">
+<div class="col l6 s12">
+<h5 class="white-text">Retail Analysis App</h5>
+<h6 class="grey-text text-lighten-4">This is Africa Data School Streamlit Class practical.</h6>
+<p class="grey-text text-lighten-4">April 2023 Cohort</p>
+</div>
+<div class="col l3 s12">
+<h5 class="white-text">Connect With Us</h5>
+<ul>
+<a href="https://www.facebook.com/AfricaDataSchool/" target="_blank" class="white-text">
+<i class="fab fa-facebook fa-4x"></i>
+</a>
+<a href="https://www.linkedin.com/company/africa-data-school" target="_blank" class="white-text">
+<i class="fab fa-linkedin fa-4x"></i>
+</a>
+<a href="https://www.youtube.com/watch?v=ZRdlQwNTJ7o" target="_blank" class="white-text">
+<i class="fab fa-youtube-square fa-4x"></i>
+</a>
+<a href="https://github.com/Africa-Data-School" target="_blank" class="white-text">
+<i class="fab fa-github-square fa-4x"></i>
+</a>
+</ul>
+</div>
+</div>
+</div>
+<div class="footer-copyright">
+<div class="container">
+Made by <a class="white-text text-lighten-3" href="https://africadataschool.com/">Regan </a><br/>
+<a class="white-text text-lighten-3" href="https://africadataschool.com/"></a>
+</div>
+</div>
+</footer>
+"""
+
+if selection == 'About':
+    st.header("About App")
+    components.html(footer_temp, height=500)
